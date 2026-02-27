e0a44db0af58c34200aa293fe4066057ec21db1a
vasun-debug <vn3339@srmist.edu.in>
2026-02-27 15:49:01 +0530

    Vasu Added UC1 Hardcode Palindrome Working Version

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,7 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         System.out.println("Welcome to Palindrome Checker App Management System");
-        System.out.println("UC2 NEW CHANGE 27");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
     }
 }
