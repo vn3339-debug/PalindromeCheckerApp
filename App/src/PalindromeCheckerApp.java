5940d8f42ed532ed97aed964866b6e136148b59d
vasun-debug <vn3339@srmist.edu.in>
2026-02-27 15:09:20 +0530

    VASU Added UC2 Hardcode Palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,6 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         System.out.println("Welcome to Palindrome Checker App Management System");
-        System.out.println("Modify this logic to usecase 2");
+        System.out.println("UC2 NEW CHANGE 27");
     }
 }
