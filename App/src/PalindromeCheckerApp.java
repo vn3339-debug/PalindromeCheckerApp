fc423f6789525e7942bb0c93ffa1e91d5d20e183
vasun-debug <vn3339@srmist.edu.in>
2026-02-27 15:00:29 +0530

    [Name] Added USECASE RELATED NAME

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,6 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker App Management System");
+        System.out.println("Modify this logic to usecase 2");
+    }
+}
